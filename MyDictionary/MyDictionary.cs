eb5f247897c4865b48da0237cfdc6e8aed0d3f59
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2021-01-28 23:05:58 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DictionaryExample
+{
+    class MyDictionary<T, E>
+    {
+        T[] _arrNumber;
+      //  E[] _arrValue; // tanımlamasına gerek olmadan da çalıştı.
+        
+
+        public MyDictionary()
+        {
+            _arrNumber = new T[0];
+            //_arrValue = new E[0];
+        }
+
+        public void Added(T item1, E item2)
+        {
+            T[] _tempNumber = _arrNumber;
+           // E[] _tempValue = _arrValue;
+
+            //_arrNumber = new T[_arrNumber.Length + 1]; // Eleman sayısını göstermek istersek yorum satırından çıkarabiliriz. 
+            //_arrValue = new E[_arrValue.Length + 1];
+
+            for (int i = 0; i < _tempNumber.Length; i++)
+            {
+
+                _arrNumber[i] = _tempNumber[i];
+                _arrNumber[_arrNumber.Length - 1] = item1;
+
+            }
+           Console.WriteLine(item1+" No'lu kullanıcı " + item2);
+          }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using DictionaryExample;
+using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> kullanicilar = new MyDictionary<int, string>();
+            kullanicilar.Added(1, "Sergen");
+            kullanicilar.Added(2, "Simge");
+            kullanicilar.Added(3, "Yaşa");
+            kullanicilar.Added(4, "Metin");
+            kullanicilar.Added(5, "Aysel");
+            kullanicilar.Added(6, "Deneme");
+            kullanicilar.Added(7, "Deneme");
+            kullanicilar.Added(8, "Deneme");
+            kullanicilar.Added(9, "Deneme");
+            kullanicilar.Added(10, "Deneme");
+            kullanicilar.Added(11, "Deneme");
+            kullanicilar.Added(12, "Deneme");
+            kullanicilar.Added(13, "Deneme");
+            kullanicilar.Added(14, "Deneme");
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
